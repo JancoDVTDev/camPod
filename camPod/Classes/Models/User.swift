fdc7a987efd11cd4f7b5e628ab5a820e90cdcf85
Janco Erasmus <jerasmus@jhb.dvt.co.za>
2020-03-02 06:50:53 +0200

    updating file structure fir bitrise

---
+++ camPod/Classes/Models/User.swift
@@ -0,0 +1,37 @@
+//
+//  User.swift
+//  camPod
+//
+//  Created by Janco Erasmus on 2020/02/27.
+//
+
+import Foundation
+
+
+public class User {
+
+    public var email: String
+    public var password: String
+    
+    public var albums: [album]?
+    
+    init(with email: String, and password: String) {
+        self.email = email
+        self.password = password
+        //Sign user in with firebase
+    }
+}
+
+public class album {
+    public var albumName: String
+    public var images: [UIImage]?
+    
+    public init(name: String, images: [UIImage]?) {
+        self.albumName = name
+        self.images = images
+    }
+    
+    public func appendImageToAlbum(image: UIImage) {
+        images?.append(image)
+    }
+}
